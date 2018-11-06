4cdc8712f18b19be2739e9e80cb5dc5e54c69bb7
marvin <marvinjameslopez>
2018-11-06 11:09:57 +0800

    Initial commit

---
+++ app/src/main/java/com/lopez/marvin/MainActivity.java
@@ -0,0 +1,65 @@
+package com.lopez.marvin;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d( "LIFECYCLE", "onCreate() has executed...");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFECYCLE", "onStart() has executed");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFECYCLE", "onResume() has executed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LIFECYCLE", "onPause() has executed");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIFECYCLE", "onStop() has executed");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LIFECYCLE", "onRestart() has executed");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LIFECYCLE", "onDestroy() has executed");
+    }
+    public void showSnack (View v){
+        Snackbar.make(v, "Next. Snackbar", Snackbar.LENGTH_LONG).show();
+    }
+
+
+    public void showToast (View v){
+        Toast.makeText(this, "Back. Toast!", Toast.LENGTH_LONG).show();
+
+    }
+
+}
